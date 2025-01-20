0ed50195c74006bef1ea245a43c7f912a62e4937
lehuygiang28 <lehuygiang28@gmail.com>
2025-01-20 21:52:20 +0700

    feat: :sparkles: update I18nMiddleware configuration to change locale order and add default locale handling

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,15 +2,31 @@ import { createI18nMiddleware } from 'next-international/middleware';
 import { NextRequest } from 'next/server';
 
 const I18nMiddleware = createI18nMiddleware({
-    locales: ['en', 'vi'],
     defaultLocale: 'vi',
+    locales: ['vi', 'en'],
     urlMappingStrategy: 'rewrite',
+    resolveLocaleFromRequest: () => {
+        // Let default locale be 'vi' if the user doesn't already have a `Next-Locale` cookie
+        return 'vi';
+    },
 });
 
+/**
+ * Middleware function that processes incoming requests using the I18nMiddleware.
+ * This function applies internationalization settings to the request based on
+ * the provided locales, default locale, and URL mapping strategy.
+ *
+ * @param request - The incoming Next.js request object.
+ * @returns The result of the I18nMiddleware processing the request.
+ */
 export function middleware(request: NextRequest) {
     return I18nMiddleware(request);
 }
 
+/**
+ * Configures the matcher for the middleware to apply to all paths except
+ * specific routes, such as API routes, static files, and built-in Next.js files.
+ */
 export const config = {
     matcher: ['/((?!api|static|.*\\..*|_next|favicon.ico|robots.txt).*)'],
 };
